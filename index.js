bf4a6e37e88e7990315ad4ab1d3457a0744e9a1a
Pavel <69979363+slemans@users.noreply.github.com>
2026-04-06 19:30:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,18 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
+const BOT_TOKEN = "8742140576:AAEbSBWqm2Ec_rRIjoSPsMiMvEvy3yS3gVQ";
+
+// проверка сервера
 app.get("/", (req, res) => {
   res.send("Bot is working 🚀");
 });
 
-app.listen(3000, () => {
-  console.log("Server started");
+// webhook endpoint
+app.post(`/bot${BOT_TOKEN}`, (req, res) => {
+  console.log("Получено сообщение от Telegram:", req.body); // <-- сюда Telegram шлёт данные
+  res.sendStatus(200);
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("Server started"));
